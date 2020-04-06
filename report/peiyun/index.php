310a544c8b238256660a5f05334f40e4d07e0c70
Justin Chang <ycchang0324@gmail.com>
2020-04-06 22:17:02 +0800

    peiyun

--- report/peiyun/index.php
+++ report/peiyun/index.php
@@ -3,7 +3,7 @@
 
 <?php
 
-    require( dirname( __FILE__ ) . './3_mail.php' ); //把要執行的檔案名稱填進這裡  
+    require( dirname( __FILE__ ) . './4_mail_and_name.php' ); //把要執行的檔案名稱填進這裡  
           
     
 
